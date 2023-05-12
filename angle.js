cab0effb37f31f78106df5d374fa998d12a51f6a
tw8130 <mwangitabitha2020@gmail.com>
2023-05-12 11:48:16 +0300

    added  angle.html to the folder and updated the angle.js to print out angles of different degrees

--- angle.js
+++ angle.js
@@ -4,3 +4,16 @@
 // Right angle: An 90 degree angle.
 // Obtuse angle: An angle between 90 and 180 degrees.
 // Straight angle: A 180 degree angle.
+
+let angle = Number(prompt("Enter angle in degrees(0-180):"));
+if (angle > 0 && angle < 90) {
+    alert("It is an Acute angle");
+} else if (angle === 90) {
+    alert(" It is a Right angle");
+} else if (angle > 90 && angle < 180) {
+    alert("It is an Obtuse angle");
+} else if (angle === 180) {
+    alert("It is a Straight angle");
+} else {
+    alert("Invalid angle. Please provide a value between 0 and 180 degrees.");
+}
